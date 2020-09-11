746ff500c037b830ee06305a53e750b53beebf25
Grig Alex <alexengrigdev@gmail.com>
2020-09-11 23:13:33 +0300

    Initial commit
    With code from example

---
+++ src/main/java/dev/alexengrig/lombok/custom/Singleton.java
@@ -0,0 +1,10 @@
+package dev.alexengrig.lombok.custom;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+public @interface Singleton {
+
+
+}
