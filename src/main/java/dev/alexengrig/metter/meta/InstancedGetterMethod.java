21b086d582249436a3fad3aab13e7fcaaf04901f
Grig Alex <alexengrigdev@gmail.com>
2020-09-14 23:04:21 +0300

    Remo meta package

--- src/main/java/dev/alexengrig/metter/meta/InstancedGetterMethod.java
+++
@@ -1,9 +0,0 @@
-package dev.alexengrig.metter.meta;
-
-import java.util.function.Supplier;
-
-@FunctionalInterface
-public interface InstancedGetterMethod<T> extends Supplier<T> {
-    @Override
-    T get();
-}
