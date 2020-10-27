63905bbdc9dd3a1eadf280182a83fe085679c70c
Grig Alex <alexengrigdev@gmail.com>
2020-10-27 21:16:58 +0300

    Create domains

---
+++ benchmark/src/main/java/dev/alexengrig/metter/benchmark/domain/Domain100.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2020 Alexengrig Dev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.alexengrig.metter.benchmark.domain;
+
+import dev.alexengrig.metter.annotation.GetterSupplier;
+import lombok.Data;
+
+@Data
+@GetterSupplier
+public class Domain100 {
+    private int int0;
+    private int int1;
+    private int int2;
+    private int int3;
+    private int int4;
+    private int int5;
+    private int int6;
+    private int int7;
+    private int int8;
+    private int int9;
+    private int int10;
+    private int int11;
+    private int int12;
+    private int int13;
+    private int int14;
+    private int int15;
+    private int int16;
+    private int int17;
+    private int int18;
+    private int int19;
+    private int int20;
+    private int int21;
+    private int int22;
+    private int int23;
+    private int int24;
+    private int int25;
+    private int int26;
+    private int int27;
+    private int int28;
+    private int int29;
+    private int int30;
+    private int int31;
+    private int int32;
+    private int int33;
+    private int int34;
+    private int int35;
+    private int int36;
+    private int int37;
+    private int int38;
+    private int int39;
+    private int int40;
+    private int int41;
+    private int int42;
+    private int int43;
+    private int int44;
+    private int int45;
+    private int int46;
+    private int int47;
+    private int int48;
+    private int int49;
+    private String string50;
+    private String string51;
+    private String string52;
+    private String string53;
+    private String string54;
+    private String string55;
+    private String string56;
+    private String string57;
+    private String string58;
+    private String string59;
+    private String string60;
+    private String string61;
+    private String string62;
+    private String string63;
+    private String string64;
+    private String string65;
+    private String string66;
+    private String string67;
+    private String string68;
+    private String string69;
+    private String string70;
+    private String string71;
+    private String string72;
+    private String string73;
+    private String string74;
+    private String string75;
+    private String string76;
+    private String string77;
+    private String string78;
+    private String string79;
+    private String string80;
+    private String string81;
+    private String string82;
+    private String string83;
+    private String string84;
+    private String string85;
+    private String string86;
+    private String string87;
+    private String string88;
+    private String string89;
+    private String string90;
+    private String string91;
+    private String string92;
+    private String string93;
+    private String string94;
+    private String string95;
+    private String string96;
+    private String string97;
+    private String string98;
+    private String string99;
+}
