2f32b6b8cf6793c59efc156345bc5af0e9ed193d
Grig Alex <alexengrigdev@gmail.com>
2021-07-16 00:55:06 +0300

    Refactor BaseMethodSupplierProcessor

--- src/main/java/dev/alexengrig/metter/processor/BaseMethodSupplierProcessor.java
+++ src/main/java/dev/alexengrig/metter/processor/BaseMethodSupplierProcessor.java
@@ -16,20 +16,19 @@
 
 package dev.alexengrig.metter.processor;
 
+import dev.alexengrig.metter.element.descriptor.ElementDescriptor;
 import dev.alexengrig.metter.element.descriptor.FieldDescriptor;
 import dev.alexengrig.metter.element.descriptor.TypeDescriptor;
 import dev.alexengrig.metter.exception.MetterException;
 import dev.alexengrig.metter.generator.MethodSupplierSourceGenerator;
 
-import javax.lang.model.SourceVersion;
-import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.Element;
 import javax.tools.JavaFileObject;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.annotation.Annotation;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -38,11 +37,14 @@ import java.util.stream.Stream;
  * Base processor of method supplier.
  *
  * @param <A> type of annotation
+ * @param <E> type of element
+ * @param <D> type of element descriptor
  * @author Grig Alex
  * @version 0.2.0
  * @since 0.1.0
  */
-public abstract class BaseMethodSupplierProcessor<A extends Annotation> extends BaseProcessor<A, TypeElement> {
+public abstract class BaseMethodSupplierProcessor<A extends Annotation, E extends Element, D extends ElementDescriptor<E>>
+        extends BaseProcessor<A, E> {
     /**
      * Source generator.
      *
@@ -70,87 +72,30 @@ public abstract class BaseMethodSupplierProcessor<A extends Annotation> extends 
     protected abstract MethodSupplierSourceGenerator getSourceGenerator();
 
     /**
-     * Process a type element.
+     * Process an element.
      *
-     * @param typeElement type element
-     * @since 0.1.0
+     * @param element element
+     * @since 0.2.0
      */
     @Override
-    protected void process(TypeElement typeElement) {
-        TypeDescriptor type = new TypeDescriptor(typeElement);
-        String sourceClassName = createSourceClassName(type);
+    protected void process(E element) {
+        D descriptor = createDescriptor(element);
+        String sourceClassName = createSourceClassName(descriptor);
         JavaFileObject sourceFile = createSourceFile(sourceClassName);
-        Map<String, String> field2Method = createField2MethodMap(type);
-        String source = createSource(type, field2Method, sourceClassName);
+        String source = createSource(sourceClassName, descriptor);
         writeSourceFile(sourceFile, source);
     }
 
-    /**
-     * Creates a source class name from a type descriptor.
-     *
-     * @param type descriptor
-     * @return source class name from {@code type}
-     * @since 0.1.0
-     */
-    protected String createSourceClassName(TypeDescriptor type) {
-        return getClassName(type).orElseGet(() -> getDefaultClassName(type));
-    }
+    protected abstract D createDescriptor(E element);
 
     /**
-     * Returns a class name from a type descriptor.
+     * Creates a source class name from a descriptor.
      *
-     * @param type descriptor
-     * @return class name from {@code type}
-     * @since 0.1.0
+     * @param descriptor descriptor
+     * @return source class name from {@code descriptor}
+     * @since 0.2.0
      */
-    protected Optional<String> getClassName(TypeDescriptor type) {
-        String customClassName = getCustomClassName(type);
-        if (customClassName.isEmpty()) {
-            return Optional.empty();
-        }
-        assertValidCustomClassName(customClassName);
-        String className = type.getQualifiedName();
-        int lastIndexOfDot = className.lastIndexOf('.');
-        if (lastIndexOfDot > 0) {
-            return Optional.of(className.substring(0, lastIndexOfDot).concat(".").concat(customClassName));
-        }
-        return Optional.of(customClassName);
-    }
-
-    /**
-     * Returns a custom class name from a type descriptor.
-     *
-     * @param type descriptor
-     * @return custom class name from {@code type}
-     * @since 0.1.0
-     */
-    protected abstract String getCustomClassName(TypeDescriptor type);
-
-    /**
-     * Asserts a valid custom class name.
-     *
-     * @param className custom class name
-     * @throws MetterException if for {@code className} {@link SourceVersion#isKeyword(java.lang.CharSequence)} returns {@code true}
-     *                         or {@link SourceVersion#isIdentifier(java.lang.CharSequence)} returns {@code false}
-     * @since 0.1.1
-     */
-    protected void assertValidCustomClassName(String className) {
-        if (SourceVersion.isKeyword(className) || !SourceVersion.isIdentifier(className)) {
-            throw new MetterException("Custom class name is invalid: '" + className + "'");
-        }
-    }
-
-    /**
-     * Returns a default class name from a type descriptor.
-     *
-     * @param type descriptor
-     * @return default class name from {@code type}
-     * @since 0.1.0
-     */
-    protected String getDefaultClassName(TypeDescriptor type) {
-        String className = type.getQualifiedName();
-        return className + annotationClass.getSimpleName();
-    }
+    protected abstract String createSourceClassName(D descriptor);
 
     /**
      * Creates a source file for a class name.
@@ -168,95 +113,14 @@ public abstract class BaseMethodSupplierProcessor<A extends Annotation> extends 
     }
 
     /**
-     * Creates a map of field to method from a type descriptor.
+     * Creates a source from a source class name and a descriptor.
      *
-     * @param type descriptor
-     * @return map of field to method from {@code type}
-     * @since 0.1.0
-     */
-    protected Map<String, String> createField2MethodMap(TypeDescriptor type) {
-        Map<String, String> field2Method = new HashMap<>();
-        Set<FieldDescriptor> fields = getFields(type);
-        for (FieldDescriptor field : fields) {
-            if (isTargetField(field)) {
-                field2Method.put(field.getName(), getMethod(field));
-            }
-        }
-        return field2Method;
-    }
-
-    /**
-     * Returns fields from a type descriptor with fields of super classes.
-     *
-     * @param type descriptor
-     * @return fields from {@code type} with fields of super classes
-     * @since 0.1.0
-     */
-    protected Set<FieldDescriptor> getFields(TypeDescriptor type) {
-        Set<TypeDescriptor> superTypes = getAllSuperTypes(type);
-        Set<FieldDescriptor> fields = Stream.concat(Stream.of(type), superTypes.stream())
-                .flatMap(descriptor -> descriptor.getFields().stream())
-                .collect(Collectors.toSet());
-        Set<String> includedFields = getIncludedFields(type);
-        Set<String> excludedFields = getExcludedFields(type);
-        if (includedFields.isEmpty() && excludedFields.isEmpty()) {
-            return fields;
-        }
-        if (!includedFields.isEmpty()) {
-            return fields.stream().filter(f -> includedFields.contains(f.getName())).collect(Collectors.toSet());
-        } else {
-            return fields.stream().filter(f -> !excludedFields.contains(f.getName())).collect(Collectors.toSet());
-        }
-    }
-
-    /**
-     * Returns included fields from a type descriptor.
-     *
-     * @param type descriptor
-     * @return included fields from {@code type}
-     * @since 0.1.0
-     */
-    protected abstract Set<String> getIncludedFields(TypeDescriptor type);
-
-    /**
-     * Returns excluded fields from a type descriptor.
-     *
-     * @param type descriptor
-     * @return excluded fields from {@code type}
-     * @since 0.1.0
-     */
-    protected abstract Set<String> getExcludedFields(TypeDescriptor type);
-
-    /**
-     * Checks if a field descriptor is target field.
-     *
-     * @param field descriptor
-     * @return {@code field} is target field.
-     * @since 0.1.0
-     */
-    protected abstract boolean isTargetField(FieldDescriptor field);
-
-    /**
-     * Returns a method for a field descriptor.
-     *
-     * @param field descriptor
-     * @return method for {@code field}
-     * @since 0.1.1
-     */
-    protected abstract String getMethod(FieldDescriptor field);
-
-    /**
-     * Creates a source from a type descriptor, a map of field to method and a source class name.
-     *
-     * @param type            descriptor
-     * @param field2Method    map of filed to method
      * @param sourceClassName source class name
-     * @return source from {@code type}, {@code field2Method} and {@code sourceClassName}
-     * @since 0.1.0
+     * @param descriptor      descriptor
+     * @return source from {@code sourceClassName} and {@code descriptor}
+     * @since 0.2.0
      */
-    protected String createSource(TypeDescriptor type, Map<String, String> field2Method, String sourceClassName) {
-        return sourceGenerator.generate(sourceClassName, type.getQualifiedName(), field2Method);
-    }
+    protected abstract String createSource(String sourceClassName, D descriptor);
 
     /**
      * Writes a source to a source file.
