6ec420edbf36a43f46be6ed3ca31b8ea1fff8259
Grig Alex <alexengrigdev@gmail.com>
2021-09-04 00:06:28 +0300

    Create InheritedSimpleDomainFactory with tests

---
+++ demo/src/main/java/dev/alexengrig/metter/demo/simple/factory/InheritedSimpleDomainFactory.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2020-2021 Alexengrig Dev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.alexengrig.metter.demo.simple.factory;
+
+import dev.alexengrig.metter.annotation.GetterSupplierFactory;
+import dev.alexengrig.metter.annotation.SetterSupplierFactory;
+import dev.alexengrig.metter.demo.Factory;
+import dev.alexengrig.metter.demo.simple.InheritedSimpleDomain;
+
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+
+public class InheritedSimpleDomainFactory implements Factory<InheritedSimpleDomain> {
+
+    @Override
+    @GetterSupplierFactory(InheritedSimpleDomain.class)
+    public Map<String, Function<InheritedSimpleDomain, Object>> getters() {
+        return new InheritedSimpleDomainGetterSupplierFactory().get();
+    }
+
+    @Override
+    @SetterSupplierFactory(InheritedSimpleDomain.class)
+    public Map<String, BiConsumer<InheritedSimpleDomain, Object>> setters() {
+        return new InheritedSimpleDomainSetterSupplierFactory().get();
+    }
+}
