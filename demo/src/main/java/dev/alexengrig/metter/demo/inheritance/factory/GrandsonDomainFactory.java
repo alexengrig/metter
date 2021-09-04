4ccfc37b7f364cf38ab441cd211fb76ae82197d7
Grig Alex <alexengrigdev@gmail.com>
2021-09-04 17:02:07 +0300

    Create GrandsonDomainFactory with tests

---
+++ demo/src/main/java/dev/alexengrig/metter/demo/inheritance/factory/GrandsonDomainFactory.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2020-2021 Alexengrig Dev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.alexengrig.metter.demo.inheritance.factory;
+
+import dev.alexengrig.metter.annotation.GetterSupplierFactory;
+import dev.alexengrig.metter.annotation.SetterSupplierFactory;
+import dev.alexengrig.metter.demo.Factory;
+import dev.alexengrig.metter.demo.inheritance.GrandsonDomain;
+
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+
+public class GrandsonDomainFactory implements Factory<GrandsonDomain> {
+    @Override
+    @GetterSupplierFactory(GrandsonDomain.class)
+    public Map<String, Function<GrandsonDomain, Object>> getters() {
+        return new GrandsonDomainGetterSupplierFactory().get();
+    }
+
+    @Override
+    @SetterSupplierFactory(GrandsonDomain.class)
+    public Map<String, BiConsumer<GrandsonDomain, Object>> setters() {
+        return new GrandsonDomainSetterSupplierFactory().get();
+    }
+}
