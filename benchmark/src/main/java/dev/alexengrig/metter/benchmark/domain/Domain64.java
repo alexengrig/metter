6f446a1b8b64681c00b97ce897645086e1b9a427
Grig Alex <alexengrigdev@gmail.com>
2021-09-12 16:02:09 +0300

    Create Domain64

---
+++ benchmark/src/main/java/dev/alexengrig/metter/benchmark/domain/Domain64.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2020-2021 Alexengrig Dev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.alexengrig.metter.benchmark.domain;
+
+import dev.alexengrig.metter.annotation.GetterSupplier;
+import lombok.Getter;
+
+@Getter
+@GetterSupplier
+public class Domain64 extends Domain32 {
+    private int int8;
+    private int int9;
+    private int int10;
+    private int int11;
+    private int int12;
+    private int int13;
+    private int int14;
+    private int int15;
+    private long long8;
+    private long long9;
+    private long long10;
+    private long long11;
+    private long long12;
+    private long long13;
+    private long long14;
+    private long long15;
+    private boolean bool8;
+    private boolean bool9;
+    private boolean bool10;
+    private boolean bool11;
+    private boolean bool12;
+    private boolean bool13;
+    private boolean bool14;
+    private boolean bool15;
+    private String string8;
+    private String string9;
+    private String string10;
+    private String string11;
+    private String string12;
+    private String string13;
+    private String string14;
+    private String string15;
+}
