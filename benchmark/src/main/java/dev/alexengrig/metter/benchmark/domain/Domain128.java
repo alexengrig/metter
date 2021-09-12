782f7a75da25b16545c175ceb79f4f5bc094cb5e
Grig Alex <alexengrigdev@gmail.com>
2021-09-12 16:21:22 +0300

    Create Domain128

---
+++ benchmark/src/main/java/dev/alexengrig/metter/benchmark/domain/Domain128.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2020-2021 Alexengrig Dev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.alexengrig.metter.benchmark.domain;
+
+import dev.alexengrig.metter.annotation.GetterSupplier;
+import lombok.Getter;
+
+@Getter
+@GetterSupplier
+public class Domain128 extends Domain64 {
+    private int int16;
+    private int int17;
+    private int int18;
+    private int int19;
+    private int int20;
+    private int int21;
+    private int int22;
+    private int int23;
+    private int int24;
+    private int int25;
+    private int int26;
+    private int int27;
+    private int int28;
+    private int int29;
+    private int int30;
+    private int int31;
+    private long long16;
+    private long long17;
+    private long long18;
+    private long long19;
+    private long long20;
+    private long long21;
+    private long long22;
+    private long long23;
+    private long long24;
+    private long long25;
+    private long long26;
+    private long long27;
+    private long long28;
+    private long long29;
+    private long long30;
+    private long long31;
+    private boolean bool16;
+    private boolean bool17;
+    private boolean bool18;
+    private boolean bool19;
+    private boolean bool20;
+    private boolean bool21;
+    private boolean bool22;
+    private boolean bool23;
+    private boolean bool24;
+    private boolean bool25;
+    private boolean bool26;
+    private boolean bool27;
+    private boolean bool28;
+    private boolean bool29;
+    private boolean bool30;
+    private boolean bool31;
+    private String string16;
+    private String string17;
+    private String string18;
+    private String string19;
+    private String string20;
+    private String string21;
+    private String string22;
+    private String string23;
+    private String string24;
+    private String string25;
+    private String string26;
+    private String string27;
+    private String string28;
+    private String string29;
+    private String string30;
+    private String string31;
+}
